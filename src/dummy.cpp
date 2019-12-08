6b92bfbf7146267eb583e71fce931305b348e876
imkiva <libkernelpanic@gmail.com>
2019-12-08 16:08:02 +0800

    cmake: dummy source to boost clion

---
+++ src/dummy.cpp
@@ -0,0 +1,7 @@
+/**
+ * Mozart++ Template Library
+ * Licensed under MIT License
+ * Copyright (c) 2019 Covariant Institute
+ * Website: https://covariant.cn/
+ * Github:  https://github.com/covariant-institute/
+ */
