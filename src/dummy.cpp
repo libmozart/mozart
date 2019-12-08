fba4e3316403a6da7b7e5c573ed25906ec98fa16
imkiva <libkernelpanic@gmail.com>
2019-12-08 16:09:27 +0800

    dummy: document

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -5,3 +5,11 @@
  * Website: https://covariant.cn/
  * Github:  https://github.com/covariant-institute/
  */
+
+/**
+ * dummy.cpp
+ *
+ * This is a dummy source file so it defines nothing and
+ * implements nothing. But CLion needs at least one source file
+ * to enable code insight and some related features.
+ */
