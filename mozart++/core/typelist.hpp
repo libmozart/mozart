57e5bd3a7fc4f49e6ca39aa2f5a78d9cafcb070f
imkiva <libkernelpanic@gmail.com>
2019-12-30 01:30:24 +0800

    code: update source header copyright

--- mozart++/core/typelist.hpp
+++ mozart++/core/typelist.hpp
@@ -1,6 +1,10 @@
-//
-// Created by kiva on 2019/12/25.
-//
+/**
+ * Mozart++ Template Library
+ * Licensed under MIT License
+ * Copyright (c) 2019 Covariant Institute
+ * Website: https://covariant.cn/
+ * Github:  https://github.com/covariant-institute/
+ */
 
 #pragma once
 
