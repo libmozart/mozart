c39708b8d566272017db8dd198ee2260b1da00da
imkiva <libkernelpanic@gmail.com>
2019-12-31 21:26:32 +0800

    test: benchmark: event emitter core: event: speed up according to benchmark

--- mozart++/core/event.hpp
+++ mozart++/core/event.hpp
@@ -68,24 +68,30 @@ namespace mpp {
             template<typename F>
             function_alias<F> *callable_ptr() {
                 using callee_arg_types = typename function_parser<function_alias<F>>::decayed_arg_types;
-                if (_args_info == typeid(callee_arg_types)) {
-                    return reinterpret_cast<function_alias<F> *>(_handler.get());
-                }
-                return nullptr;
-            }
 
-            function_alias<void()> *callable_ptr() {
-                // Directly check argument count
-                // because _args_info == typeid(typelist::nil) is much slower here.
-                if (_args_count == 0) {
-                    return reinterpret_cast<function_alias<void()> *>(_handler.get());
+                // When callee didn't pass any argument,
+                // we only need to check _arg_count.
+                // Avoid typeid() call as much as possible.
+                //
+                // Note that:
+                // This branch condition is always a constexpr, so don't
+                // worry about the branch overhead.
+                //
+                if (mpp::typelist::empty_v<callee_arg_types>) {
+                    if (_args_count == 0) {
+                        return reinterpret_cast<function_alias<F> *>(_handler.get());
+                    }
+                } else {
+                    if (_args_info == typeid(callee_arg_types)) {
+                        return reinterpret_cast<function_alias<F> *>(_handler.get());
+                    }
                 }
                 return nullptr;
             }
         };
 
     private:
-        std::unordered_map<std::string, std::vector<handler_container>> _events;
+        std::unordered_map<std::string, std::forward_list<handler_container>> _events;
 
     public:
         event_emitter_fast() = default;
@@ -101,7 +107,7 @@ namespace mpp {
          */
         template<typename Handler>
         void on(const std::string &name, Handler handler) {
-            _events[name].push_back(handler_container(handler));
+            _events[name].push_front(handler_container(handler));
         }
 
         /**
