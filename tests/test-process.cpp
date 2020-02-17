3bb73f481ab008600be6ea1cde1e7cdb87762d00
imkiva <libkernelpanic@gmail.com>
2020-02-17 16:11:04 +0800

    process: fix tests on *nix

--- tests/test-process.cpp
+++ tests/test-process.cpp
@@ -87,7 +87,6 @@ void test_r_file() {
 
     process p = process_builder().command(SHELL)
 #ifndef MOZART_PLATFORM_WIN32
-        .arguments(std::vector<std::string>{"-NonInteractive"})
         .environment("VAR", "fuckcpp")
 #endif
         .redirect_stdout(fileno(fout))
